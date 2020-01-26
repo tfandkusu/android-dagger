cdc7c37670cb4904018a4122e8728e366c520ae0
Haruhiko Takada <haruhiko.takada@gmail.com>
2020-01-27 00:58:59 +0900

    2つのフラグメントでDIを使う

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -3,6 +3,8 @@ package com.example.android.dagger.di
 import android.content.Context
 import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationActivity
+import com.example.android.dagger.registration.enterdetails.EnterDetailsFragment
+import com.example.android.dagger.registration.termsandconditions.TermsAndConditionsFragment
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
@@ -20,6 +22,10 @@ interface AppComponent {
     fun inject(activity: RegistrationActivity)
 
     fun inject(activity: MainActivity)
+
+    fun inject(fragment: EnterDetailsFragment)
+
+    fun inject(fragment: TermsAndConditionsFragment)
 
 
     /**
